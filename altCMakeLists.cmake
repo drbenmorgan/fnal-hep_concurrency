1aba7203597b852c67f2b4d8996dd59170188140
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2017-10-26 16:54:28 +0100

    Bump minimum cetbuildtools2 to 0.2.0
    Remove FindTBB module, use targets from cetbuildtools2 FindTBB.

--- altCMakeLists.cmake
+++ altCMakeLists.cmake
@@ -2,7 +2,7 @@ cmake_minimum_required(VERSION 3.3)
 project(hep_concurrency VERSION 0.1.0)
 
 # - Cetbuildtools, version2
-find_package(cetbuildtools2 0.1.0 REQUIRED)
+find_package(cetbuildtools2 0.2.0 REQUIRED)
 set(CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR} ${cetbuildtools2_MODULE_PATH})
 set(CET_COMPILER_CXX_STANDARD_MINIMUM 14)
 include(CetInstallDirs)
--- hep_concurrency/altCMakeLists.cmake
+++ hep_concurrency/altCMakeLists.cmake
@@ -20,9 +20,8 @@ target_include_directories(hep_concurrency
   PUBLIC
    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}>
    $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
-   ${TBB_INCLUDE_DIRS}
    )
-target_link_libraries(hep_concurrency PUBLIC ${TBB_LIBRARIES})
+target_link_libraries(hep_concurrency PUBLIC TBB::tbb)
 
 # ======================================================================
 # Testing
